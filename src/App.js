30fc2e7cd66c5cb7e3cd70797c839306ea8b3abd
sriramvemparala17 <sriramvemparala17@gmail.com>
2022-09-15 19:13:03 -0700

    lab1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CMPE281 – Cloud Technology</h1>
+        <h2>Lab #1 Assignment – Playing with Amazon’s EC2</h2>
+        <p>
+          Deploying an application into AWS Cloud and monitoring performance and resource usage of application using Amazon’s CloudWatch.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/asgn.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class asgn extends Component {
+    render(){
+        return (
+            <div className="App">
+                 <h1>skdhc shv </h1>
+            </div>
+          );
+    }
+    
+}
+
+export default asgn;
